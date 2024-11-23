363779dd10338d470b7e796bbf73d26973af4cdf
Egor Paroshin <eparoshin@gmail.com>
2024-11-23 15:43:13 +0300

    wip

--- 1/gen_graph.py
+++ 1/gen_graph.py
@@ -1,7 +1,7 @@
 import math
 
 with open('output.txt', 'w') as f:
-    num_steps = int((1000.0 - 0.0) / 0.001) + 1
+    num_steps = int((100000.0 - 0.0) / 0.001) + 1
     for i in range(num_steps):
         x = i * 0.001
         y = math.cos(x) ** 2
